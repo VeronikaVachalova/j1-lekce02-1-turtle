4a97376b56a4ef7d5d26f2f10eb363a1f9e6fbd2
VeronikaVachalova <veronika.vachalova@email.cz>
2022-04-17 10:41:06 +0200

    Zofka napsala jmeno Verca.

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -241,5 +241,121 @@ public class HlavniProgram {
         zofka.turnLeft(115);
         zofka.move(40);
 
+        //jmeno
+
+        zofka.penUp();
+        zofka.turnRight(160);
+
+        //pismeno V
+        zofka.setLocation(600, 400);
+        zofka.turnLeft(20);
+        zofka.penDown();
+        zofka.move(40);
+        zofka.turnRight(180);
+        zofka.move(40);
+        zofka.turnLeft(130);
+        zofka.move(43);
+        zofka.penUp();
+        zofka.turnRight(180);
+        zofka.move(40);
+        zofka.turnLeft(120);
+        zofka.move(20);
+
+        // posunuti
+        zofka.move(20);
+        zofka.penDown();
+
+        // pismeno E
+        zofka.turnLeft(90);
+        zofka.move(40);
+        zofka.turnRight(90);
+        zofka.move(30);
+        zofka.turnLeft(180);
+        zofka.penUp();
+        zofka.move(30);
+        zofka.turnLeft(90);
+        zofka.move(20);
+
+        zofka.turnLeft(90);
+        zofka.penDown();
+        zofka.move(30);
+
+        zofka.turnLeft(180);
+        zofka.penUp();
+        zofka.move(30);
+        zofka.turnLeft(90);
+        zofka.move(20);
+
+        zofka.turnLeft(90);
+        zofka.penDown();
+        zofka.move(30);
+
+        // posunuti
+        zofka.penUp();
+        zofka.move(20);
+        zofka.penDown();
+
+        //pismeno R
+        zofka.turnLeft(90);
+        zofka.move(40);
+        zofka.turnRight(45);
+
+        for (int i = 0; i < 60; i++) {
+            zofka.turnRight(4);
+            zofka.move(1);
+        }
+        zofka.turnRight(200);
+        zofka.move(30);
+
+        //pismeno C
+        zofka.penUp();
+        zofka.turnLeft(40);
+        zofka.move(40);
+        zofka.turnLeft(80);
+        zofka.move(30);
+
+
+        zofka.penDown();
+        for (int i = 0; i < 46; i++) {
+            zofka.turnLeft(4);
+            zofka.move(1);
+        }
+        zofka.move(30);
+        zofka.turnLeft(30);
+
+        zofka.penDown();
+        for (int i = 0; i < 40; i++) {
+            zofka.turnLeft(4);
+            zofka.move(1);
+        }
+        //posunuti
+        zofka.penUp();
+        zofka.turnRight(180);
+        zofka.move(17);
+        zofka.turnLeft(80);
+
+        zofka.move(15);
+
+        //pismeno A
+        zofka.turnLeft(70);
+        zofka.penDown();
+        zofka.move(50);
+        zofka.turnLeft(225);
+        zofka.move(52);
+
+        zofka.turnLeft(180);
+        zofka.penUp();
+        zofka.move(16);
+        zofka.turnLeft(70);
+        zofka.penDown();
+        zofka.move(15);
+        zofka.move(14);
+
+        //posunuti
+        zofka.penUp();
+        zofka.turnLeft(50);
+        zofka.move(30);
+        zofka.turnRight(230);
+
     }
 }
